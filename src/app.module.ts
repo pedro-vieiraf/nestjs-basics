13b8acfd6849f76758d9d9a501de1a57a554c9dc
Pedro Vieira <pedrovieirafdev@gmail.com>
2025-09-15 10:49:30 -0300

    feat: implementing TypeORM configurations in app.module.ts

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,9 +1,23 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { BooksModule } from './books/books.module';
 
 @Module({
-  imports: [],
+  imports: [
+    TypeOrmModule.forRoot({
+      type: 'postgres',
+      host: process.env.DB_HOST || 'db',
+      port: Number(process.env.DB_PORT) || 5432,
+      username: process.env.DB_USER || 'postgres',
+      password: process.env.DB_PASS || 'postgres',
+      database: process.env.DB_NAME || 'myapp',
+      autoLoadEntities: true,
+      synchronize: true,
+    }),
+    BooksModule,
+  ],
   controllers: [AppController],
   providers: [AppService],
 })

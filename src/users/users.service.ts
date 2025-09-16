55e2214d8a4ac97b85cc08b1383546ce5892b48d
Pedro Vieira <pedrovieirafdev@gmail.com>
2025-09-16 10:12:20 -0300

    feat: setting up User configurations

--- src/users/users.service.ts
+++ src/users/users.service.ts
@@ -1,4 +1,30 @@
-import { Injectable } from '@nestjs/common';
+import { Injectable, NotFoundException } from '@nestjs/common';
+import { CreateUserDto } from './dto/create-user.dto';
+import { InjectRepository } from '@nestjs/typeorm';
+import { User } from './entities/user.entity';
+import { Repository } from 'typeorm/repository/Repository';
 
 @Injectable()
-export class UsersService {}
+export class UsersService {
+  constructor(
+    @InjectRepository(User)
+    private usersRepository: Repository<User>,
+  ) {}
+
+  create(CreateUserDto: CreateUserDto) {
+    const user = this.usersRepository.create(CreateUserDto);
+    return this.usersRepository.save(user);
+  }
+
+  async findOne(username: string): Promise<User | undefined> {
+    const user = await this.usersRepository.findOneBy({ username });
+    if (!user) {
+      throw new NotFoundException(`User with username ${username} not found`);
+    }
+    return user;
+  }
+
+  async findOneOptional(username: string): Promise<User | null> {
+    return this.usersRepository.findOneBy({ username });
+  }
+}

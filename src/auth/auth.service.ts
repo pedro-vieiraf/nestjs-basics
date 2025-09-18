721aec2378332e7ad88fdacb1dc4a08934aa59be
Pedro Vieira <pedrovieirafdev@gmail.com>
2025-09-18 15:09:29 -0300

    fixing type errors

--- src/auth/auth.service.ts
+++ src/auth/auth.service.ts
@@ -1,6 +1,7 @@
 import { Injectable, UnauthorizedException } from '@nestjs/common';
 import { UsersService } from '../users/users.service';
 import { JwtService } from '@nestjs/jwt';
+import { SignInResponseDto } from './dto/sign-in.dto';
 
 @Injectable()
 export class AuthService {
@@ -9,10 +10,7 @@ export class AuthService {
     private jwtService: JwtService,
   ) {}
 
-  async signIn(
-    username: string,
-    pass: string,
-  ): Promise<{ access_token: string }> {
+  async signIn(username: string, pass: string): Promise<SignInResponseDto> {
     const user = await this.usersService.findOne(username);
     if (user.password !== pass) {
       throw new UnauthorizedException();
